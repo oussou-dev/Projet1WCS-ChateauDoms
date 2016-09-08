1375319566fa37f48b1a4856c4215830039ccd7f
Sebastien <sbstnplcn@gmail.com>
2016-09-08 18:10:14 +0200

    filter animate

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 // Smooth Scroll
     $('a[href^="#"]:not(.modal-trigger)').click( function() {
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top-50
+            scrollTop: $( $(this).attr('href') ).offset().top-0
         }, 1000);
         return false;
     });
@@ -19,25 +19,25 @@ $(document).ready(function() {
     $('#filter').find('#showRouge').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRose, #showBlanc, #showAll').removeClass('active');
-        $('#wine').find('#rose, #blanc').addClass('hide');
-        $('#wine').find('#rouge1, #rouge2, #rouge3').removeClass('hide');
+        $('#wine').find('#rose, #blanc').hide( "slow" );
+        $('#wine').find('#rouge1, #rouge2, #rouge3').show('slow');
     });
     $('#filter').find('#showRose').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRouge, #showBlanc, #showAll').removeClass('active');
-        $('#wine').find('#rouge1, #rouge2, #rouge3, #blanc').addClass('hide');
-        $('#wine').find('#rose').removeClass('hide');
+        $('#wine').find('#rouge1, #rouge2, #rouge3, #blanc').hide( "slow" );
+        $('#wine').find('#rose').show('slow');
     });
     $('#filter').find('#showBlanc').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRose, #showRouge, #showAll').removeClass('active');
-        $('#wine').find('#rose, #rouge1, #rouge2, #rouge3').addClass('hide');
-        $('#wine').find('#blanc').removeClass('hide');
+        $('#wine').find('#rose, #rouge1, #rouge2, #rouge3').hide( "slow" );
+        $('#wine').find('#blanc').show('slow');
     });
     $('#filter').find('#showAll').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRose, #showBlanc, #showRouge').removeClass('active');
-        $('#wine').find('#blanc, #rose, #rouge1, #rouge2, #rouge3').removeClass('hide');
+        $('#wine').find('#blanc, #rose, #rouge1, #rouge2, #rouge3').show('slow');
     });
 
 
