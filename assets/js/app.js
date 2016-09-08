c512857cf70d111847d50c02124a39c6d8e80964
Sebastien <sbstnplcn@gmail.com>
2016-09-08 18:04:53 +0200

    css + scrolltotop + design

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 //Init Materialize
     $('.modal-trigger').leanModal();
     $(".button-collapse").sideNav();
-    $('#closeModal').closeModal();
+
 
 // Smooth Scroll
     $('a[href^="#"]:not(.modal-trigger)').click( function() {
@@ -16,29 +16,43 @@ $(document).ready(function() {
 
 
 //Filter Page Products
-    $('#filter').find('#showRouge').on('click', function(){
+    $('#filter').find('#showRouge').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRose, #showBlanc, #showAll').removeClass('active');
         $('#wine').find('#rose, #blanc').addClass('hide');
         $('#wine').find('#rouge1, #rouge2, #rouge3').removeClass('hide');
     });
-    $('#filter').find('#showRose').on('click', function(){
+    $('#filter').find('#showRose').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRouge, #showBlanc, #showAll').removeClass('active');
         $('#wine').find('#rouge1, #rouge2, #rouge3, #blanc').addClass('hide');
         $('#wine').find('#rose').removeClass('hide');
     });
-    $('#filter').find('#showBlanc').on('click', function(){
+    $('#filter').find('#showBlanc').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRose, #showRouge, #showAll').removeClass('active');
         $('#wine').find('#rose, #rouge1, #rouge2, #rouge3').addClass('hide');
         $('#wine').find('#blanc').removeClass('hide');
     });
-    $('#filter').find('#showAll').on('click', function(){
+    $('#filter').find('#showAll').click(function(){
         $(this).addClass('active');
         $('#filter').find('#showRose, #showBlanc, #showRouge').removeClass('active');
         $('#wine').find('#blanc, #rose, #rouge1, #rouge2, #rouge3').removeClass('hide');
     });
+
+
+
+//Scroll to top
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+
 
 
 
