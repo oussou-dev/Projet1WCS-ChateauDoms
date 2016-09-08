c7bb6f6424ba22212f389b70d093894a6b6353c7
Sebastien <sbstnplcn@gmail.com>
2016-09-08 15:48:54 +0200

    page product responsive + css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,44 @@
 $(document).ready(function() {
 
-		/* Smooth Scroll*/
-    $('a[href^="#"]').click( function() {
+//Init Materialize
+    $('.modal-trigger').leanModal();
+    $(".button-collapse").sideNav();
+    $('#closeModal').closeModal();
+
+// Smooth Scroll
+    $('a[href^="#"]:not(.modal-trigger)').click( function() {
         $('html, body').animate({
             scrollTop: $( $(this).attr('href') ).offset().top-50
         }, 1000);
         return false;
-	});
-
-    $('.modal-trigger').leanModal();
+    });
 
 
 
-        $('#filter').find('#showRouge').on('click', function(){
-            $('#wine').find('#rose, #blanc').addClass('hide');
-            $('#wine').find('#rouge1, #rouge2, #rouge3').removeClass('hide');
-        });
-        $('#filter').find('#showRose').on('click', function(){
-            $('#wine').find('#rouge1, #rouge2, #rouge3, #blanc').addClass('hide');
-            $('#wine').find('#rose').removeClass('hide');
-        });
-        $('#filter').find('#showBlanc').on('click', function(){
-            $('#wine').find('#rose, #rouge1, #rouge2, #rouge3').addClass('hide');
-            $('#wine').find('#blanc').removeClass('hide');
-        });
-        $('#filter').find('#showAll').on('click', function(){
-            $('#wine').find('#blanc, #rose, #rouge1, #rouge2, #rouge3').removeClass('hide');
-        });
+//Filter Page Products
+    $('#filter').find('#showRouge').on('click', function(){
+        $(this).addClass('active');
+        $('#filter').find('#showRose, #showBlanc, #showAll').removeClass('active');
+        $('#wine').find('#rose, #blanc').addClass('hide');
+        $('#wine').find('#rouge1, #rouge2, #rouge3').removeClass('hide');
+    });
+    $('#filter').find('#showRose').on('click', function(){
+        $(this).addClass('active');
+        $('#filter').find('#showRouge, #showBlanc, #showAll').removeClass('active');
+        $('#wine').find('#rouge1, #rouge2, #rouge3, #blanc').addClass('hide');
+        $('#wine').find('#rose').removeClass('hide');
+    });
+    $('#filter').find('#showBlanc').on('click', function(){
+        $(this).addClass('active');
+        $('#filter').find('#showRose, #showRouge, #showAll').removeClass('active');
+        $('#wine').find('#rose, #rouge1, #rouge2, #rouge3').addClass('hide');
+        $('#wine').find('#blanc').removeClass('hide');
+    });
+    $('#filter').find('#showAll').on('click', function(){
+        $(this).addClass('active');
+        $('#filter').find('#showRose, #showBlanc, #showRouge').removeClass('active');
+        $('#wine').find('#blanc, #rose, #rouge1, #rouge2, #rouge3').removeClass('hide');
+    });
 
 
 
